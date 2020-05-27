ecb712641cc4a59f24b2fa653e428e6b34318add
Irene Scott <irenejinscott@gmail.com>
2020-05-27 17:38:21 +0000

    setting up more methods in cli and api to give outcomes to the if statement

--- lib/api.rb
+++ lib/api.rb
@@ -2,13 +2,16 @@ require "pry"
 class API
   #make calls to API
 
-  def self.get_jokes
+  def self.get_programming_jokes
     url = "https://official-joke-api.appspot.com/jokes/programming/ten"
     uri = URI(url)
     response = Net::HTTP.get(uri)
     hash_of_jokes = JSON.parse(response)
 
-
+#General, programming, knock knock
+#https://official-joke-api.appspot.com/jokes/knock-knock/ten
+#https://official-joke-api.appspot.com/jokes/general/ten
+#https://official-joke-api.appspot.com/jokes/programming/ten
 
     hash_of_jokes.each do |joke_hash|
         joke_instance = Joke.new
@@ -22,12 +25,41 @@ class API
     #kind of joke => "type"
     #question => "setup"
     #punchline => "punchline"
-
-
-
 #initialize a new joke
 #assign attributes to it
 #repeat above method with a different url
+    end
+  end
+
+  def self.get_knock_knock_jokes
+    url = "https://official-joke-api.appspot.com/jokes/knock-knock/ten"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    hash_of_jokes = JSON.parse(response)
+
+    hash_of_jokes.each do |joke_hash|
+        joke_instance = Joke.new
+
+        joke_instance.id = joke_hash["id"]
+        joke_instance.kind = joke_hash["type"]
+        joke_instance.question = joke_hash["setup"]
+        joke_instance.punchline = joke_hash["punchline"]
+    end
+  end
+
+  def self.get_general_jokes
+    url = "https://official-joke-api.appspot.com/jokes/general/ten"
+    uri = URI(url)
+    response = NET::HTTP.get(uri)
+    hash_of_jokes = JSON.parse(response)
+
+    hash_of_jokes.each do |joke_hash|
+        joke_instance = Joke.new
+
+        joke_instance.id = joke_hash["id"]
+        joke_instance.kind = joke_hash["type"]
+        joke_instance.question = joke_hash["setup"]
+        joke_instance.punchline = joke_hash["punchline"]
     end
   end
 
--- lib/cli.rb
+++ lib/cli.rb
@@ -5,33 +5,47 @@ class CLI
 
   def start
     puts "Welcome"
-    API.get_jokes
-    binding.pry
-    self.feelings
+    API.get_programming_jokes
+    self.do_you_want_programming_joke
   end
 
-  def feelings
-    puts "Do you want to hear a joke that is about animals?"
+  def do_you_want_programming_joke
+    puts "Hey unhappy person I see that look on your face. You sure could go
+    for a smile right now. Why don’t we make a cheese sandwhich with
+    your smile in the middle and 2 jokes to be the bread pieces to hold it all
+    together? We’ll ask you a few questions to get the right jokes for what you're in the mood for.
+    Do you want to hear a joke that is about programming?"
+    puts "It's going to be a yes or a no"
     user_input = gets.strip
     if user_input == "yes"
-      puts "Yes you do want to hear a 'sad' joke"
-      displays_random_sad_jokes
-    elsif
-      #end the program
-      puts "What about a joke that relates to you feeling angry?"
-      displays_random_angry_jokes
-    else
-      puts "What about a joke that relates to you feeling confused?"
-      displays_random_confused_jokes
+      puts "Ah wonderful choice, hope you work in software so that you can get these."
+      API.get_programming_jokes
+    else user_input == "no"
+      puts "Okay let's try something else."
+      self.do_you_want_knock_knock_joke
     end
-
   end
 
-  def displays_sad_jokes
-    #access all the jokes
-    #print 3 jokes
-    Joke.all.each do |joke|
-      puts joke.id
+  def do_you_want_knock_knock_joke
+    puts "Hmm what about a knock-knock joke?"
+    user_input = gets.strip
+    if user_input == "yes"
+      puts "Glad we could find something you like, you'll be knocking on your friends' doors trying these"
+      API.get_knock_knock_jokes
+    else
+      puts "Okay let's try something else."
+      self.do_you_want_general_joke
+    end
+  end
+
+  def do_you_want_general_joke
+    puts "What about a joke that is about anything and everything?"
+    if user_input == "yes"
+      puts "Finally! Okay here they are..."
+     API.get_general_jokes
+    else
+      puts "That's all we have. Try again later."
+      #end program
     end
   end
 
