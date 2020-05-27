0772407fee590a44a6bdcd7ae4fed5cfabe6084e
Irene Scott <irenejinscott@gmail.com>
2020-05-27 00:06:20 +0000

    message here

--- lib/api.rb
+++ lib/api.rb
@@ -3,19 +3,19 @@ class API
   #make calls to API
 
   def self.get_jokes
-    url = "https://github.com/15Dkatz/official_joke_api/blob/master/jokes/index.json"
+    url = "https://official-joke-api.appspot.com/jokes/programming/ten"
     uri = URI(url)
     response = Net::HTTP.get(url)
-    hash = JSON.parse(response)
-
+    JSON.parse(response)
+    # something here then each do |variable|
+    #   initialize a new jokes
+    #   new_instance = Joke.new
+    #    assign attributes to it
+    #   this is where you put the stuff from the api
+    #   new_instance.id = (hash of elements)[]
     #****tried to run ruby bin/start, also not saving my new information
+  binding.pry
 
-    something here then each do |variable|
-      #initialize a new jokes
-      new_instance = Joke.new
-      # assign attributes to it
-      #this is where you put the stuff from the api
-      new_instance.id = (hash of elements)[]
   end
 
 end
--- lib/jokes.rb
+++ lib/jokes.rb
@@ -1,6 +1,6 @@
 class Joke
   #keep track of joke attributes and save all jokes created
-  attr_accessor :#from the json response after we use pry put the attributes here i.e. :name, :category
+#from the json response after we use pry put the attributes here i.e. :name, :category
   @@all = []
   def initialize
     @@all << self
