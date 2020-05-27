27184884c16afc3787308475644398797bee9da0
Irene Scott <irenejinscott@gmail.com>
2020-05-27 23:31:07 +0000

    set methods that will allow me to display the correct jokes whether a user says yes or no

--- lib/cli.rb
+++ lib/cli.rb
@@ -9,38 +9,55 @@ class CLI
     self.do_you_want_programming_joke
   end
 
+  def display_programming_joke
+     joke = Joke.programming.sample
+     puts joke.question
+     puts joke.punchline
+#sleep
+     #Joke.programming.each do |joke|
+  end
+
+  def display_knock_knock_joke
+     joke = Joke.knock_knock.sample
+#The problem is whenever I try to get it to use a knock knock jokes it will not display anything whether it is a 
+#yes or a no, I think it is because it is pulling it out of the programming url not the knock knock url
+     puts joke.question
+     puts joke.punchline
+  end
+
+  def display_general_joke
+     joke = Joke.general.sample
+     puts joke.question
+     puts joke.punchline
+  end
+
   def do_you_want_programming_joke
     puts "Hey unhappy person I see that look on your face. You sure could go
     for a smile right now. Why don’t we make a cheese sandwhich with
     your smile in the middle and 2 jokes to be the bread pieces to hold it all
-    together? We’ll ask you a few questions to get the right jokes for what you're in the mood for.
-    Do you want to hear a joke that is about programming?"
+    together? We’ll ask you a few questions to get the right jokes for what you're
+    in the mood for. Do you want to hear a joke that is about programming?"
     puts "It's going to be a yes or a no"
     user_input = gets.strip
     if user_input == "yes"
       puts "Ah wonderful choice, hope you work in software so that you can get these."
-      display_programming_jokes
+      display_programming_joke
     else user_input == "no"
       puts "Okay let's try something else."
-      self.do_you_want_knock_knock_joke
+      do_you_want_knock_knock_joke
     end
   end
 
-  def display_programming_joke
-    Joke.all.each do |joke|
-      puts joke
-    end
-  end
 
   def do_you_want_knock_knock_joke
     puts "Hmm what about a knock-knock joke?"
     user_input = gets.strip
     if user_input == "yes"
       puts "Glad we could find something you like, you'll be knocking on your friends' doors trying these"
-      API.get_knock_knock_jokes
-    else
+      display_knock_knock_joke
+    else user_input == "no"
       puts "Okay let's try something else."
-      self.do_you_want_general_joke
+      do_you_want_general_joke
     end
   end
 
@@ -48,8 +65,8 @@ class CLI
     puts "What about a joke that is about anything and everything?"
     if user_input == "yes"
       puts "Finally! Okay here they are..."
-     API.get_general_jokes
-    else
+      display_general_joke
+   else user_input == "no"
       puts "That's all we have. Try again later."
       #end program
     end
--- lib/jokes.rb
+++ lib/jokes.rb
@@ -12,4 +12,28 @@ attr_accessor :id, :kind, :question, :punchline
     @@all
   end
 
+  #add some class methods to the Joke class -
+
+  #Essentially, you’ll need to use the .find_all or .select method
+  #on ALL of the jokes to just select programming ones
+
+ def self.programming
+  all.select do |joke_instance| #this has to match with the something.kind
+    joke_instance.kind == "programming"
+    end
+  end
+
+  def self.knock_knock
+   all.select do |knocking_joke_instance|
+     knocking_joke_instance.kind == "knock-knock"
+     #had binding.pry here
+     end
+   end
+
+    def self.general
+      all.select do |general_joke_instance|
+        general_joke_instance.kind == "general"
+        end
+      end
+
 end
