ee471f01bb66a184589f65c6ce48cab76b48cde8
Irene Scott <irenejinscott@gmail.com>
2020-06-05 18:05:20 +0000

    added a new method that turns ls into exclamation marks in the punchline

--- lib/jokes.rb
+++ lib/jokes.rb
@@ -11,8 +11,6 @@ attr_accessor :id, :kind, :question, :punchline
     @punchline = punchline
     @@all << self
   end
-#create an instance method for the joke class that will change the punchline in some way, change the jokes punchline so that you replace all of the ls with
-#exclamation marks, call it punchline, if punchline does not have any l's in it then 3 exclaimation marks at the end of the line
 
 def special_punchline
   if punchline.include? "l"
